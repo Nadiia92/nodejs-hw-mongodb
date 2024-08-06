2396e2bc47b48bfabcf695cd87e8af78c15cc2fc
Nadiia Khrystenko <nadiiakhrystenko1000@gmail.com>
2024-08-06 16:23:29 +0300

    hw-4

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,9 +6,24 @@ import {
   deleteContact,
   updateContact,
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export async function getAllContactsController(req, res) {
-  const contacts = await getAllContactsFromDB();
+  const { page, perPage } = parsePaginationParams(req.query);
+
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
+  const filter = parseFilterParams(req.query);
+
+  const contacts = await getAllContactsFromDB({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
   res.json({
     status: 200,
     message: 'Successfully fond contacts!',
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,43 @@
 import { Contact } from '../db/models/Contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContactsFromDB = () => Contact.find();
+export const getAllContactsFromDB = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+
+  if (filter.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+  const contactsCount = await Contact.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
 
 export const getContactByIdFromDB = (contactId) => Contact.findById(contactId);
 
@@ -9,7 +46,13 @@ export const createContact = (contactData) => Contact.create(contactData);
 export const deleteContact = (contactId) =>
   Contact.findOneAndDelete({ _id: contactId });
 
-export const updateContact = (contactId, payload) =>
+export const updateContact = (
+  contactId,
+  payload,
+  options = {
+    upsert: true,
+  },
+) =>
   Contact.findOneAndUpdate(
     {
       _id: contactId,
@@ -17,5 +60,6 @@ export const updateContact = (contactId, payload) =>
     payload,
     {
       new: true,
+      ...options,
     },
   );
